47d078b3d82f6839f4706da5c50dfcdcdc4bc233
edipo <edipocb@gmail.com>
2021-07-12 22:13:11 -0300

    Primeiro commit

---
+++ features/support/login.feature
@@ -0,0 +1,13 @@
+#language: pt
+# frozen_string_literal: true
+
+Funcionalidade: Realizar o login
+     Para que o cliente consiga acessar a minha conta
+     Sendo um cliente cadastrado no site
+     Desejo que o cliente consiga acessar suas reservas 
+
+  @login
+  Cenário: Acessar a minha conta
+     Dado que o cliente tenha cadastro no site
+     Quando o cliente acessar seu cadastro com <email> e <senha>
+     Entao valido que o <pedido> está legível para solicitar o reembolso
---
+++ features/support/pages/login_page.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+# Representa a pagina login
+class LoginPage < SitePrism::Page
+  element  :btn_close_modal,    '.btn-close-modal'
+  element  :modal_checkout,     '#checkoutAbandon'
+  element  :btn_entrar,         '#btn-acesse-3'
+  element  :cadastre_se,        '#btn-cadastro'
+  element  :inserir_email,      '#emailsuper'
+  element  :inserir_senha,      '#senhasuper'
+  element  :botao_entrar,       '#btacessesuaconta'
+  element  :nome_cliente,       '.name-user'
+  element  :minhas_reservas,    '#perfil-link-reservas'
+  element  :numero_pedido,      '.ng-binding'
+  element  :cancelar_reserva,   '#btnCancelOrder' # 'a[class=btn-md]'
+
+  include Capybara::DSL
+
+  def acessar_site
+    visit '/'
+    btn_close_modal.click if has_modal_checkout?
+    btn_entrar.click
+    cadastre_se.click
+  end
+
+  def acessar_conta(email, senha)
+    inserir_email.set email
+    inserir_senha.set senha
+    botao_entrar.click
+    sleep 5
+  end
+
+  def cliente_logado
+    nome_cliente.click
+    minhas_reservas.click
+  end
+
+  def localizar_pedido(numero_pedido)
+    all('.bx-reserva').each do |reserva|
+      within(reserva) do
+        numero = find('h2').text.gsub(/[a-zA-Z ]/, '')
+        return cancelar_reserva.click if numero == numero_pedido
+      end
+    end
+  end
+end
